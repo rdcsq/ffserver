58c9572c45eec811f8989a1807bb5698b919c9d6
rdcsq <git@rdcsq.me>
2024-03-18 19:31:35 -0600

    feat: implement jwt authentication

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module ffserver
 go 1.22.1
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"ffserver/api"
 	"ffserver/env"
 	"ffserver/ffmpeg"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func main() {
@@ -22,8 +26,39 @@ func main() {
 	http.ListenAndServe(env.ListeningAddress, nil)
 }
 
+type JwtPayload struct {
+	Source string
+	jwt.RegisteredClaims
+}
+
 func middleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		jwtString := strings.Replace(r.Header.Get("Authorization"), "Bearer ", "", 1)
+		if jwtString == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.ParseWithClaims(jwtString, &JwtPayload{}, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("error decoding JWT")
+			}
+			return env.AuthSecret, nil
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(*JwtPayload)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		log.Printf("auth,%v\n", claims.Source)
+
 		w.Header().Set("Content-Type", "application/json")
 
 		handler.ServeHTTP(w, r)
